cae379a27947dee50c39fdcdb2f1b69c4c93d04e
javaAndroid <sveridovdenys@yandex.ua>
2013-05-31 10:53:01 +0300

    Two commit auth test

--- www/index.php
+++ www/index.php
@@ -1,16 +1,41 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+$json['code'] = 99;
+
+if (isset($_POST['type'])) {
+    /*
+     * Registration
+     */
+    if ($_POST['type'] == 'registration') {
+        if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['name'])) {
+            
+        }
+    }
+    /*
+     * AUTH
+     */
+    if ($_POST['type'] == 'auth') {
+        if (isset($_POST['email']) && isset($_POST['password'])) {
+            getToken($_POST['email'], $_POST['password']);
+        }
+    }
+}
+
+echo json_encode($json);
+
+function getToken($email, $password) {
+    $mysqli = new mysqli('127.0.0.1', 'root', '', 'messenger');
+    if (mysqli_connect_errno()) {
+        echo "Подключение невозможно: " . mysqli_connect_error();
+    }
+   echo "SELECT * FROM users WHERE email='".$email."',password='".$password."';";
+   $query = $mysqli->query("SELECT * FROM users WHERE email='".$email."' password='".$password."';");
+   print_r($query);
+    $mysqli->close();
+}
+
+function registration($email, $password, $name) {
+    
+}
+
+?>
